920e08e8ce5ea5c366eb8fb8e9d64b9945c7a154
Jimmy Nilsson <jimmy.nilsson@hotmail.se>
2021-09-21 17:51:07 +0200

    updated with close ml server

--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 import requests
 from datetime import datetime
 import sqlite3
+import subprocess
+import sys
 
 class MLModel():
     modelname = ["question_answering", "text_generator", "sentiment_analysis", "image_classifier"]
@@ -10,12 +12,21 @@ class MLModel():
         self.modeltype = modeltype
         self.r = requests.Response
         self.text = str
+        self.p = subprocess.Popen
+        self.out = {}
 
     def start(self):
         selected_model = {"name" : self.modeltype}
         endpoint= self.app + "/start/"
         self.r = requests.post(url=endpoint, json=selected_model)
         print(self.r.status_code)
+
+    def run_server(self):
+        sub_args=[sys.executable, 'src/main.py']
+        self.p = subprocess.Popen(sub_args,stdin=subprocess.PIPE, stdout=subprocess.PIPE, creationflags = subprocess.CREATE_NEW_CONSOLE,shell=True)
+
+    def terminate_server(self):
+        subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=self.p.pid))
 
 class MLTextGenerator(MLModel):
     def __init__(self, modeltype: str = "text_generator", app: str = "http://localhost:8000") -> None:
@@ -50,11 +61,12 @@ class MLSentimentAnalysis(MLModel):
         endpoint = (self.app + "/sentiment_analysis/")
         self.r = requests.post(url=endpoint, json=context)
         result, score = self._format_text(self.r.text)
-        return{"date": str(datetime.now()),
-               "modeltype": self.modeltype,
-               "context": text,
-               "result": result,
-               "score": score}
+        self.out = {"date": str(datetime.now()),
+                    "modeltype": self.modeltype,
+                    "context": text,
+                    "result": result,
+                    "score": score}
+        return self.out
 
     def _format_text(self, text):
         if "POSITIVE" in text:
@@ -77,12 +89,13 @@ class MLQA(MLModel):
         context_question = {"context": context, "question": question}
         endpoint = (self.app + "/qa/")
         self.r = requests.post(url=endpoint, json=context_question)
-        return{"date": str(datetime.now()),
-               "modeltype": self.modeltype,
-               "context": context,
-               "result": self.r.text.split(":")[1][:-8],
-               "score": self.r.text.split(":")[-1][:-1],
-               "question": question}
+        self.out = {"date": str(datetime.now()),
+                    "modeltype": self.modeltype,
+                    "context": context,
+                    "result": self.r.text.split(":")[1][:-8],
+                    "score": self.r.text.split(":")[-1][:-1],
+                    "question": question}
+        return self.out
 
 class MLImageClassifier(MLModel):
     def __init__(self, modeltype: str = "image_classifier", app: str = "http://localhost:8000") -> None:
@@ -98,11 +111,12 @@ class MLImageClassifier(MLModel):
             self._change_classes(classes)
         files = {'file': open(file_path, 'rb')}
         self.r = requests.post(url=self.endpoint, files=files)
-        return{"date": str(datetime.now()),
-        "context" : file_path.split("/")[-1],
-        "modeltype": self.modeltype,
-        "result": self.r.text,
-        "image": self._create_blob(file_path)}
+        self.out = {"date": str(datetime.now()),
+                    "filename" : file_path.split("/")[-1],
+                    "modeltype": self.modeltype,
+                    "result": self.r.text,
+                    "image": self._create_blob(file_path)}
+        return self.out
 
     def _create_blob(self,filepath : str):
         # Convert digital data to binary format
@@ -111,9 +125,12 @@ class MLImageClassifier(MLModel):
         return blobData
 
 class databaseDB():
-    def __init__(self, db_loc = '../data/dbgroup2.db') -> None:
+    def __init__(self, db_loc = 'data/dbgroup2.db') -> None:
         self.db_loc = db_loc
         self.db = sqlite3.Connection
+
+    def createTables():
+        pass
 
     def _connect(self):
         self.db = sqlite3.connect(self.db_loc)
